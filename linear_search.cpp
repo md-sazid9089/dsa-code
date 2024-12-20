dedbe363895c3f6d9eecbfcb0ab87fe9d9c86ca0
syed mohammed sazid ullah <mdsazid9089@gmail.com>
2024-12-20 20:10:30 +0600

    added all

---
+++ linear_search.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int linearSearch(int arr[], int n, int target) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == target) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main() {
+    int arr[] = {10, 20, 30, 40, 50};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int target = 30;
+
+    int index = linearSearch(arr, n, target);
+    if (index != -1) {
+        cout << "Element found at index: " << index << endl;
+    } else {
+        cout << "Element not found" << endl;
+    }
+
+    return 0;
+}
+
---
+++ practice.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int arr[]={1,2,3,4,5,6,7,8};
+int n=sizeof(n);
+int target=3;
+for(int i=0;i<n;i++){
+    if(arr[i]==target) printf("found");
+    else printf("not found");
+}
+
+
+}
