7967d865bb4d562d5e75f641447d9f4f4f8f5693
Syed Mohammed Sazid Ullah <164257013+md-sazid9089@users.noreply.github.com>
2024-12-28 19:55:58 +0600

    Update binary_search_recursion.cpp

--- binary_search_recursion.cpp
+++ binary_search_recursion.cpp
@@ -1,3 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main()
+int BInary_searach(vector<int>&arr,int start,int end1,int item){
+
+while(start<=end1){
+    int mid=(start+end1)/2;
+    if(arr[mid]>item) return BInary_searach(arr,start,mid-1,item);
+    else if(arr[mid]<item) return BInary_searach(arr,mid+1,end1,item);
+    else return mid;
+}return -1;
+
+}
+int main(){
+vector<int>v;
+for(iterator:: it=v.begin();it<v.end();it++){
+    int x;
+    cin>>x.push_back(*it);
+}
+int result=BInary_searach(v;0;v.size-1;8);
+if(!result) cout<<"found"<<endl;
+else cout<<"not found"<<endl;
+return 0;
+
+
+
+}
