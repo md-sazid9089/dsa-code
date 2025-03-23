8cf9090dd4d6d7e58a3f2cc8126bbe584f28eb9f
syed mohammed sazid ullah <mdsazid9089@gmail.com>
2025-03-23 16:24:11 +0600

    a

---
+++ find_maximum.cpp
@@ -0,0 +1,44 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+struct Node{
+int data;
+Node* left;
+Node* right;
+};
+struct Node* FindMax(struct Node* root){
+while(root->right!=NULL){
+    root=root->right;
+}
+return root;
+};
+struct Node* Insert(struct Node* root,int data){
+if(root == NULL) {
+		root = new Node();
+		root->data = data;
+		root->left = root->right = NULL;
+	}
+	else if(data <= root->data)
+		root->left = Insert(root->left,data);
+	else
+		root->right = Insert(root->right,data);
+	return root;
+};
+void Inorder(struct Node* root) {
+    if (root == NULL) return;
+    Inorder(root->left);
+    cout << root->data << " ";
+    Inorder(root->right);
+}
+int main(){
+	Node* root = NULL;
+	root = Insert(root,5); root = Insert(root,10);
+	root = Insert(root,3); root = Insert(root,4);
+	root = Insert(root,1); root = Insert(root,11);
+    cout<<"Inorder: ";
+	Inorder(root);
+	cout<<"\n";
+struct Node* maxNode=FindMax(root);
+	cout<<"maximum is   "<<maxNode->data<<" ";
+
+}
